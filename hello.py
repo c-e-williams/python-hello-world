10827a2744599c0dd77d84af7581ea8e47d54952
Cathleen <cathleen@MacBook-Pro-2.local>
2024-08-19 17:37:41 -0500

    <Catching up>

--- hello.py
+++ hello.py
@@ -3,14 +3,41 @@
 # Enter your code on line 7 to print 'Hello, World!'
 # to the console (the panel on the right)
 
-print("Hello");
+# print("Hello");
+# print ("Hello World");
+# print "Hello World"
+# print ("Hello World)
+# print (6)
+# print (6+6)
+# print ('coding is fun', 'launchcode is cool', 'I am sleepy')
+# print  ('coding is fun')
+# print ('launchcode is cool')
+# print ('I am sleepy')
+
+
+       
+# print ('Quoth the Raven "Nevermore"')
+
+# day = "Thursday"
+# day = 32.5
+# day = 19
+# print(day)
+
+# info = input("Please enter your age: ")
+# # The user enters 25.
+
+# print(type(info))
+# int('3')
+# int('three')
 
 # Next, PLAY!
 # Modify your code on line 7 to try to accomplish the tasks listed below:
 
 #   1. Change the message that is printed.
-#   2. Figure out what the parentheses do. Will the code work without them?
+#   2. Figure out what the parentheses do. Will the code work without them? 
+#      No, the code doesn't work without the parenthesis. 
 #   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+        # Yes we need to include quotation marks.  We can use single or double quotes; it doesn't matter which.
 #   4. Print a number. (Bonus: Print two numbers added together).
 #   5. Print multiple messages one after the other.
 #   6. Print two messages on the same line.
